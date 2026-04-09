a4f32e01b7af6ff5f4334eb69f897808201a7c85
Dana <pradanawahyuhidayanto@gmail.com>
2026-04-09 15:19:52 +0700

    logo denso welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,11 +20,7 @@
     <div class="d-flex flex-column h-100">
         <div>
             <div class="brand-lockup fade-in-up">
-                <span class="brand-lockup__icon">SEC</span>
-                <div>
-                    <h2 class="brand-lockup__title">{{ $brand['name'] ?? 'DENSO' }}</h2>
-                    <p class="brand-lockup__subtitle">{{ $brand['tagline'] ?? 'Crafting the Core' }}</p>
-                </div>
+                <img src="{{ asset('storage/Red_DENSO_Hires.png') }}" alt="DENSO - Crafting the Core" class="brand-lockup__logo">
             </div>
 
             <div class="sidebar-section fade-in-up delay-1">
